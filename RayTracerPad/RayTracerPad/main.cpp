8aa15eded0fdde62c1d38f5d0c8ed7021e01f1a1
ming zi <410888305@qq.com>
2017-10-22 17:38:19 -0600

    Chapter1: Output an image(in .ppm).

---
+++ RayTracerPad/RayTracerPad/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+	ofstream ppm;
+	ppm.open("image.ppm");
+	int nx = 200;
+	int ny = 100;
+	ppm << "P3\n" << nx << " " << ny << "\n255\n";
+	for (int j = ny - 1; j >= 0; j--) {
+		for (int i = 0; i < nx; i++) {
+			float r = float(i) / float(nx);
+			float g = float(j) / float(ny);
+			float b = 0.2;
+			int ir = int(255.99 * r);
+			int ig = int(255.99 * g);
+			int ib = int(255.99 * b);
+			ppm << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+	return 0;
+}
